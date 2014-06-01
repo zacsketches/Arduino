56a40bf84a2c7ddea68b5b304d26966de4f0f36f
Zac Staples <zacstaples@mac.com>
2014-06-01 12:19:59 -0700

    First refacton on data structures used by the scanner
    created a new Scan_points class and a test driver

---
+++ Dev_tests/data_struc.cpp
@@ -0,0 +1,37 @@
+/*  On 6/1/2014 I committed to getting my Alfred robot up and running again.
+    This time I want to take what I've leared about C++ and build a few data
+    structures that support an easy implementation for a servo that rotates and
+    takes ultrasonic measurements.
+
+    This is a non-arduino compatible c++ driver to test my library data structures.
+*/
+
+#include "Scanner.h"
+#include <iostream>
+
+void test_scan_point() {
+  cout<<"************************************************************\n";
+  cout<<"TESTING SCAN_POINT\n";
+
+  //default construct a scan_point
+  Scan_point a(100);
+  cout<<"a's heading is: "<<a.heading()<<" and a's data is: "<<a.data()<<endl;
+  cout<<"setting heading\n";
+  a.set_heading(90);
+  cout<<"a's heading is: "<<a.heading()<<" and a's data is: "<<a.data()<<endl;
+  
+  
+  //construct a scan point with two params provided
+  Scan_point b(100, 50000);
+  cout<<"b's heading is: "<<b.heading()<<" and b's data is: "<<b.data()<<endl;
+  cout<<"setting data\n";
+  b.set_data(100000)
+  cout<<"b's heading is: "<<b.heading()<<" and b's data is: "<<b.data()<<endl;
+}
+
+int main (int argc, char const *argv[])
+{
+  test_scan_point();
+  return 0;
+}
+
--- Scanner.h
+++ Scanner.h
@@ -41,72 +41,66 @@
 #ifndef Scanner_h
 #define Scanner_h
 
-#include <inttypes.h>
+#include "Servo.h"
 
-/* 
- * Defines for 16 bit timers used with  Servo library 
- *
- * If _useTimerX is defined then TimerX is a 16 bit timer on the curent board
- * timer16_Sequence_t enumerates the sequence that the timers should be allocated
- * _Nbr_16timers indicates how many 16 bit timers are available.
- *
- */
-
-// Say which 16 bit timers can be used and in what order
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-#define _useTimer5
-#define _useTimer1 
-#define _useTimer3
-#define _useTimer4 
-typedef enum { _timer5, _timer1, _timer3, _timer4, _Nbr_16timers } timer16_Sequence_t ;
-
-#elif defined(__AVR_ATmega32U4__)  
-#define _useTimer1 
-typedef enum { _timer1, _Nbr_16timers } timer16_Sequence_t ;
-
-#elif defined(__AVR_AT90USB646__) || defined(__AVR_AT90USB1286__)
-#define _useTimer3
-#define _useTimer1
-typedef enum { _timer3, _timer1, _Nbr_16timers } timer16_Sequence_t ;
-
-#elif defined(__AVR_ATmega128__) ||defined(__AVR_ATmega1281__)||defined(__AVR_ATmega2561__)
-#define _useTimer3
-#define _useTimer1
-typedef enum { _timer3, _timer1, _Nbr_16timers } timer16_Sequence_t ;
-
-#else  // everything else
-#define _useTimer1
-typedef enum { _timer1, _Nbr_16timers } timer16_Sequence_t ;                  
-#endif
-
-#define Servo_VERSION           2      // software version of this library
-
-#define MIN_PULSE_WIDTH       544     // the shortest pulse sent to a servo  
-#define MAX_PULSE_WIDTH      2400     // the longest pulse sent to a servo 
-#define DEFAULT_PULSE_WIDTH  1500     // default pulse width when servo is attached
-#define REFRESH_INTERVAL    20000     // minumim time to refresh servos in microseconds 
-
-#define SERVOS_PER_TIMER       12     // the maximum number of servos controlled by one timer 
-#define MAX_SERVOS   (_Nbr_16timers  * SERVOS_PER_TIMER)
-
-#define INVALID_SERVO         255     // flag indicating an invalid servo index
-
-typedef struct  {
-  uint8_t nbr        :6 ;             // a pin number from 0 to 63
-  uint8_t isActive   :1 ;             // true if this channel is enabled, pin not pulsed if false 
-} ServoPin_t   ;  
-
-typedef struct {
-  ServoPin_t Pin;
-  unsigned int ticks;
-} servo_t;
-
-class Scanner
-{
+class Scan_point{
+ 	int h;		        //heading in degrees...nominal range if [0 : 180]
+ 	long elem;        //most recent reading at this point
 public:
-  Scanner();
-  uint8_t attach(int pin, int pingPin);      // attach the given pin to the next free channel, sets pinMode, returns channel number or 0 if failure
-  long measure_angle(int heading);           // return distance measurement at angle provided
+  Scan_point(const int heading, const long data = 0)
+    :h(heading), elem(data) {}
+   	
+  int heading() const { return h; }
+  void set_heading(int head) { h = head; }
+
+  long data() const { return elem; }
+  void set_data( long dat ){ elem = data; }
+};
+
+class Scan {
+  int sp;                         //number of Scan_points in the scan
+  int spn;                       //degrees to scan (constrained by servo max physical limit)
+  Scan_point elem[];
+
+public:  
+  Scan(const int scan_points, const int span, const int center);
+  ~Scan() {delete[] elem; }
+  
+  // NON-MODIFYING METHODS //
+  int size() const { return sp; }
+  int span() const { return spn; }
+  char[] const headings();        //JSON format
+  char[] const data();            //JSON format
+  
+  
+};
+
+/* commented out so I can test my data structures first
+
+class Scanner {
+private: 
+  //Servo data
+  int sp;               //Servo pin
+  int ctr;              //where the servo points directly ahead of the bot
+  Servo servo;          //Servo object
+
+  //Ultrasonic sensor data
+  int pp;               //ping_pin
+
+  //Scan object handles data for the scanner
+  Scan  scan;           
+
+public:
+  //constructor
+  Scanner(const int servo_pin, 
+          const int ping_pin, 
+          const int center = 90,
+          const int span = 180,
+          const int test_points = 5);
+  
+  char[] headings();
+
+  long measure_angle(int heading);    // return distance measurement at angle provided
   void detach();
   void write(int value);             // if value is < 200 its treated as an angle, otherwise as pulse width in microseconds 
   void writeMicroseconds(int value); // Write pulse width in microseconds 
@@ -119,12 +113,8 @@ public:
   long microsecondsToCentimeters(const long microseconds);	//convert pulse duration to distance
 private:
   double find_delay(int target_angle); 	//compute delay for servo to travel to target heading
-  int ping_pin;                       	// data pin for PING))) sensor
-  int span;								//degrees
-  int test_points;						//number of locations the scanner stops to take a meaurement
-  int span_center;						//degrees
   int scan_points[5];					//array of angles measurements are taken at
-  int scan_delay;						//milliseconds
+  int scan_delay;						  //milliseconds
   int pulse_delay;              		//milliseconds
   double servo_angular_rate;		    //Seconds per degree.
  
@@ -132,5 +122,7 @@ private:
   int8_t min;                       // minimum is this value times 4 added to MIN_PULSE_WIDTH    
   int8_t max;                       // maximum is this value times 4 added to MAX_PULSE_WIDTH   
 };
+
+*/
 
 #endif
